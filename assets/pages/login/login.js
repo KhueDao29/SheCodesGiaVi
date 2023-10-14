9dde26d141571b45636b7b55ff76b9dd1fbcc94e
Khue Dao <you@example.com>
2023-10-14 19:37:25 +0700

    update assets

---
+++ assets/pages/login/login.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const user = {
+    username: "lasacmam",
+    password: "mat5tieng",
+}
+
+const Login = () => {
+    return (
+        <View><Text>trasuakhonghanh</Text></View>
+    )
+}
+
+export default Login;
